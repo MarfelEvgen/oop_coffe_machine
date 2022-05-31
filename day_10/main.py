d9cd405f984e37986d14641864a71fd8bc9d9242
MarfelEvgen <mormarfel2@gmail.com>
2022-05-31 14:01:30 +0300

    add new functions

--- day_10/main.py
+++ day_10/main.py
@@ -3,6 +3,14 @@ import random
 from game_data import data
 from art import *
 
+def random_choice():
+    return random.choice(data)
+
+def seleb_data(account):
+    name = account['name']
+    description = account['description']
+    country = account['country']
+    return f'{name}, a {description}, from {country}'
 
 def win_seleb(a, b):
     if a > b:
@@ -10,26 +18,35 @@ def win_seleb(a, b):
     else:
         return b
 
+def double_check(a, b):
+    if a == b:
+        b = random_choice()
+    return b
+
 def game_name():
-    seleb_a = random.choice(data)
-    seleb_b = random.choice(data)
-    follower_a = seleb_a['follower_count']
-    follower_b = seleb_b['follower_count']
     game_continue = True
-    p = win_seleb(follower_a, follower_b)
     score = 0
 
     while game_continue:
-        print(f"Compare A: {seleb_a['name']}, a {seleb_a['description']}, from {seleb_a['country']}{follower_a}")
+        seleb_a = random_choice()
+        seleb_b = random_choice()
+
+        double_check(seleb_a, seleb_b)
+
+        follower_a = seleb_a['follower_count']
+        follower_b = seleb_b['follower_count']
+        guess = win_seleb(follower_a, follower_b)
+
+        print(f"Compare A: {seleb_data(seleb_a)}.")
         print(vs)
-        print(f'Compare B: {seleb_b["name"]}, a {seleb_b["description"]}, from {seleb_b["country"]}{follower_b}')
+        print(f"Compare A: {seleb_data(seleb_b)}.")
         answer = input("'Who has more followers? Type 'a' or 'b':\n")
         if answer == 'a':
             answer = follower_a
         else:
             answer = follower_b
 
-        if answer == p:
+        if answer == guess:
             score += 1
             print(f'Wery good.\nCurrent score: {score}')
             clear()
@@ -38,10 +55,8 @@ def game_name():
             print(f"Sorry, that's wrong.\nFinal score: {score}")
             game_continue = False
 
-
 print(logo)
 game_name()
-
 
 
 
